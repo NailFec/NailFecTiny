e6a4b9dcda026c455ca6f3e25f8d0fb557c01d10
NailFec <nail-17@outlook.com>
2024-10-15 20:11:11 +0800

    fix and add func in wordle-guess

--- wordle-guess/main.cpp
+++ wordle-guess/main.cpp
@@ -1,14 +1,11 @@
 // wordle - guess
-// by NailFec 20241014 finished in a rush
-// TODO: guess more possible words first
 #include <bits/stdc++.h>
 using namespace std;
-bool without[26], isnt[26];
+bool without[26], is[26], isnt[26];
 struct ndStatus {
     // 0: don't know; -1: without; 1: isn't; 2: is
     int mode;
-    int is;
-    bool isnt[5];
+    bool is[5], isnt[5];
 } status[26];
 vector<string> words;
 
@@ -23,36 +20,58 @@ int main() {
     printf("loaded %llu words\n", words.size());
     string guess = "nails";
     for(int iRole = 0; iRole < 6; iRole ++) {
+        debug:
         printf("times #%d: ", iRole);
         cout << guess << endl;
-        printf("wordle result (mode): ");
+        printf("wordle result (mode):");
         for(int i = 0, ia; i < 5; i ++) {
             scanf("%d", &ia);
+            if(ia == 27) {
+                printf("change word:");
+                cin >> guess;
+                goto debug;
+            }
             status[guess[i] - 'a'].mode = ia;
-            if(ia == -1) without[guess[i] - 'a'] = true;
-            else if(ia == 1) status[guess[i] - 'a'].isnt[i] = true, isnt[guess[i] - 'a'] = true;
-            else if(ia == 2) status[guess[i] - 'a'].is = i;
-            else return 0 * printf("NailERROR: invalid input mode\n");
+            if(ia == -1)
+                without[guess[i] - 'a'] = true;
+            else if(ia == 1)
+                isnt[guess[i] - 'a'] = true, status[guess[i] - 'a'].isnt[i] = true;
+            else if(ia == 2)
+                is[guess[i] - 'a'] = true, status[guess[i] - 'a'].is[i] = true;
+            else
+                return 0 * printf("NailERROR: invalid input mode\n");
         }
-        cout << endl;
         for(string tryS : words) {
             bool can = true;
-            for(int i = 0; i < 26; i ++) if(isnt[i]) {
-                bool consist = false;
-                for (const char c : tryS)
-                    if(c - 'a' == i) { consist = true; break; }
-                if(!consist) { can = false; break; }
-            }
-            for(const char c : tryS) {
-                if(without[c - 'a']) { can = false; break; }
-                if(status[c - 'a'].mode == 0) continue;
-                if(status[c - 'a'].mode == 2)
-                    if(tryS[status[c - 'a'].is] != c) { can = false; break; }
-                if(status[c - 'a'].mode == 1)
-                    for(int i = 0; i < 5; i ++)
-                        if(status[c - 'a'].isnt[i] && tryS[i] == c) { can = false; break; }
+            for(int i = 0; i < 26; i ++) {
+                if(without[i]) {
+                    bool consist = false;
+                    for(const char c : tryS)
+                        if(c - 'a' == i) { consist = true; break; }
+                    if(consist) { can = false; break; }
+                }
+                // if(!can) break;
+                if(is[i]) {
+                    for(int j = 0; j < 5; j ++) {
+                        if(status[i].is[j])
+                            if(tryS[j] - 'a' != i) { can = false; break; }
+                    }
+                }
+                if(!can) break;
+                if(isnt[i]) {
+                    bool consist = false;
+                    for(const char c : tryS)
+                        if(c - 'a' == i) { consist = true; break; }
+                    if(!consist) { can = false; break; }
+                    for(int j = 0; j < 5; j ++) {
+                        if(status[i].isnt[j])
+                            if(tryS[j] - 'a' == i) { can = false; break; }
+                    }
+                }
+                if(!can) break;
             }
             if(can) {
+                if(iRole == 5) cout << "> " << tryS << endl;
                 guess = tryS;
                 vector<bool> checker(26, false);
                 bool great = true;
